1283c7a56eaa0ef4443cf659dad81b0d02e3d83d
‪ENG. YAHYA SHALEF‬‏ <engyahyashalef@gmail.com>
2026-06-02 00:27:12 +0300

    Add files via upload

---
+++ image_steganography.py
@@ -0,0 +1,10 @@
+img=open("2.jpg","rb")
+img_read=img.read()
+print(img_read)
+
+
+
+file=open("2.jpg","a")
+file.write("...............>>>>> name : yahya shalef <<<<<...........")
+file.close()
+print("encod")
